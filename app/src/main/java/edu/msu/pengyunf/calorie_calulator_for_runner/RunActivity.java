1f3e846730c5c4ac8c817f348bcf1a17fc4fa108
Junshan Luo <luojunsh@cse.msu.edu>
2017-04-29 02:26:34 -0400

    已写好距离计算，貌似刷新的频率有点问题

--- app/src/main/java/edu/msu/pengyunf/calorie_calulator_for_runner/RunActivity.java
+++ app/src/main/java/edu/msu/pengyunf/calorie_calulator_for_runner/RunActivity.java
@@ -10,10 +10,16 @@ import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class RunActivity extends AppCompatActivity {
 
     private double latitude = 0;
     private double longitude = 0;
+    private double inital_latitude = 0;
+    private double inital_longitude = 0;
+    private float[] distance = new float[1];
+    private Location location;
     private LocationManager locationManager = null;
 
     private ActiveListener activeListener = new ActiveListener();
@@ -29,6 +35,20 @@ public class RunActivity extends AppCompatActivity {
         // Force the screen to say on and bright
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        distance[0] = 0;
+        registerListeners();
+        setUI();
+
+
+    }
+    private void setUI()
+    {
+        TextView lat = (TextView)findViewById(R.id.textLat);
+        TextView log = (TextView)findViewById(R.id.textLong);
+        TextView dis = (TextView)findViewById(R.id.textDistance);
+        lat.setText(String.valueOf(inital_latitude));
+        log.setText(String.valueOf(inital_longitude));
+        dis.setText(String.valueOf(distance[0]));
     }
 
     @Override
@@ -41,6 +61,19 @@ public class RunActivity extends AppCompatActivity {
     protected void onPause() {
         unregisterListeners();
         super.onPause();
+    }
+    private void getinitallocation()
+    {
+        inital_latitude = latitude;
+        inital_longitude = longitude;
+    }
+
+    private float[] calculateDistance()
+    {
+        float[] result = new float[1];
+        result[0] = 0;
+        Location.distanceBetween(inital_latitude,inital_longitude,latitude,longitude,result);
+        return result;
     }
 
     private void registerListeners() {
@@ -60,8 +93,14 @@ public class RunActivity extends AppCompatActivity {
 
         if(bestAvailable != null) {
             try{
-                locationManager.requestLocationUpdates(bestAvailable, 5000, 15, activeListener);
+                locationManager.requestLocationUpdates(bestAvailable, 5, 5, activeListener);
                 Location location = locationManager.getLastKnownLocation(bestAvailable);
+                if(inital_latitude==0 && inital_longitude==0)
+                {
+                    inital_latitude = location.getLatitude();
+                    inital_longitude = location.getLongitude();
+                    setUI();
+                }
             }
             catch(SecurityException e){
                 System.out.println("SecurityException");
@@ -80,10 +119,20 @@ public class RunActivity extends AppCompatActivity {
 
         @Override
         public void onLocationChanged(Location location) {
+            inital_latitude = latitude;
+            inital_longitude = longitude;
             latitude = location.getLatitude();
             longitude = location.getLongitude();
-            System.out.println("location changed: latitude is " + location.getLatitude() + ", longitude is " + longitude);
+            if(inital_longitude == 0.0 && inital_latitude==0.0)
+            {
+                inital_longitude = longitude;
+                inital_latitude = latitude;
+            }
+            distance[0] += calculateDistance()[0];
+            setUI();
+            System.out.println("location changed: latitude is " + latitude  + ", longitude is " + longitude);
             //do distance calculate here
+
 
         }
 
@@ -101,6 +150,6 @@ public class RunActivity extends AppCompatActivity {
         public void onProviderDisabled(String provider) {
             registerListeners();
         }
-    };
+    }
 
 }
